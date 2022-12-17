6a4663098191f4d2640722166935de9ddf3ade7f
Mark Pan <markpan@live.com>
2022-12-17 17:41:40 +0800

    Add User scaffold

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,6 +1,8 @@
 Rails.application.config.generators do |g|
   g.helper = false
+  g.jbuilder = false
   g.orm :active_record, primary_key_type: :uuid
   g.request_specs false
-  g.system_specs true
+  g.routing_specs false
+  g.view_specs false
 end
